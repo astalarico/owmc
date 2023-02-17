e0ff24e40732ece80abfa20d5ff72d5900d83a61
Anthony Talarico <astalarico@gmail.com>
2023-02-16 22:42:49 -0500

    adding ground tilesets

--- Assets/Scripts/init.gd
+++ Assets/Scripts/init.gd
@@ -9,6 +9,7 @@ extends Node
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+	print('load')
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
