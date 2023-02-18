38d963d63415b1a7698ee5f41bb13091ad753a22
Anthony Talarico <astalarico@gmail.com>
2023-02-17 20:39:58 -0500

    adding texture directly to the viewport in the control_camera scene to test for distortion

--- Assets/Scripts/init.gd
+++ Assets/Scripts/init.gd
@@ -8,8 +8,8 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	#$ViewportContainer/Viewport/Planet_1/Camera2D.zoom = Vector2(0.8,.8)
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
